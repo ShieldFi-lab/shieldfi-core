b8eccf36675c25f471f9a694b2858a1d7b08d419
Cipher <shieldfiofficial@gmail.com>
2026-03-10 12:00:26 +0200

    Update ClaimManager.sol

--- contract/ClaimManager.sol
+++ contract/ClaimManager.sol
@@ -1,1 +1,14 @@
-.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ClaimManager {
+
+    event ClaimProcessed(address user, uint256 amount);
+
+    function processClaim(address user, uint256 amount) public {
+
+        emit ClaimProcessed(user, amount);
+
+    }
+
+}
