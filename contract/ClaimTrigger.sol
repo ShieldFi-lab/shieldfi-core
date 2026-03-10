7652e2f4660da8db8b4d610542f5581564699ebd
Cipher <shieldfiofficial@gmail.com>
2026-03-10 11:33:19 +0200

    Update ClaimTrigger.sol

--- contract/ClaimTrigger.sol
+++ contract/ClaimTrigger.sol
@@ -1,1 +1,21 @@
-.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ClaimTrigger {
+
+    bool public triggerActivated;
+
+    event TriggerActivated(string triggerType, uint256 timestamp);
+
+    function activateTrigger(string memory triggerType) public {
+
+        triggerActivated = true;
+
+        emit TriggerActivated(triggerType, block.timestamp);
+    }
+
+    function resetTrigger() public {
+        triggerActivated = false;
+    }
+
+}
