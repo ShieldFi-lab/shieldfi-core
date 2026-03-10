a612f71ca806e9586ed9de7805d6b35078a667db
Cipher <shieldfiofficial@gmail.com>
2026-03-10 12:01:00 +0200

    Update PremiumManager.sol

--- contract/PremiumManager.sol
+++ contract/PremiumManager.sol
@@ -1,1 +1,14 @@
-.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract PremiumManager {
+
+    event PremiumPaid(address user, uint256 amount);
+
+    function payPremium() public payable {
+
+        emit PremiumPaid(msg.sender, msg.value);
+
+    }
+
+}
