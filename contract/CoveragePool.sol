04f6335c558b5aaee43ac0f23b92047426985eb8
Cipher <shieldfiofficial@gmail.com>
2026-03-10 11:19:19 +0200

    Update CoveragePool.sol

--- contract/CoveragePool.sol
+++ contract/CoveragePool.sol
@@ -1,1 +1,25 @@
-.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract CoveragePool {
+
+    mapping(address => uint256) public liquidity;
+
+    uint256 public totalLiquidity;
+
+    function deposit() public payable {
+        liquidity[msg.sender] += msg.value;
+        totalLiquidity += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+
+        require(liquidity[msg.sender] >= amount, "Not enough balance");
+
+        liquidity[msg.sender] -= amount;
+        totalLiquidity -= amount;
+
+        payable(msg.sender).transfer(amount);
+    }
+
+}
